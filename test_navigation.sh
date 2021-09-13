c26327679f28fd7d23596eae800a884c6120d51f
mantues <45273263+mantues@users.noreply.github.com>
2021-09-13 12:41:35 +0900

    Add files via upload

--- test_navigation.sh
+++ test_navigation.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 xterm  -e  "source /opt/ros/kinetic/setup.bash" &
 xterm  -e  "source ~/Desktop/turtleworld/devel/setup.bash" &
-xterm  -e  "export TURTLEBOT3_MODEL=waffle" &
 sleep 5
+xterm  -e  "source /opt/ros/kinetic/setup.bash" &
+xterm  -e  "source ~/Desktop/turtleworld/devel/setup.bash" &
+xterm  -e  "export TURTLEBOT3_MODEL=waffle" &
 xterm  -e  "roslaunch myrobot turtlebotwithworld.launch" & 
 sleep 5
+xterm  -e  "source /opt/ros/kinetic/setup.bash" &
+xterm  -e  "source ~/Desktop/turtleworld/devel/setup.bash" &
+xterm  -e  "export TURTLEBOT3_MODEL=waffle" &
 xterm  -e  " roslaunch myrobot navigation.launch" 
--- test_slam.sh
+++ test_slam.sh
@@ -3,8 +3,17 @@ xterm  -e  "source /opt/ros/kinetic/setup.bash" &
 xterm  -e  "source ~/Desktop/turtleworld/devel/setup.bash" &
 xterm  -e  "export TURTLEBOT3_MODEL=waffle" &
 sleep 5
+xterm  -e  "source /opt/ros/kinetic/setup.bash" &
+xterm  -e  "source ~/Desktop/turtleworld/devel/setup.bash" &
+xterm  -e  "export TURTLEBOT3_MODEL=waffle" &
 xterm  -e  "roslaunch myrobot turtlebotwithworld.launch" & 
 sleep 5
+xterm  -e  "source /opt/ros/kinetic/setup.bash" &
+xterm  -e  "source ~/Desktop/turtleworld/devel/setup.bash" &
+xterm  -e  "export TURTLEBOT3_MODEL=waffle" &
 xterm  -e  "roslaunch turtlebot3_slam turtlebot3_slam.launch" & 
 sleep 5
+xterm  -e  "source /opt/ros/kinetic/setup.bash" &
+xterm  -e  "source ~/Desktop/turtleworld/devel/setup.bash" &
+xterm  -e  "export TURTLEBOT3_MODEL=waffle" &
 xterm  -e  "roslaunch turtlebot3_teleop turtlebot3_teleop_key.launch" 
