5e3cbcd2239d3c5917abc2d23f11613007ee503e
mantues <45273263+mantues@users.noreply.github.com>
2021-09-13 12:12:20 +0900

    Add files via upload

---
+++ test_navigation.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+xterm  -e  "source /opt/ros/kinetic/setup.bash" &
+xterm  -e  "source ~/Desktop/turtleworld/devel/setup.bash" &
+xterm  -e  "export TURTLEBOT3_MODEL=waffle" &
+sleep 5
+xterm  -e  "roslaunch myrobot turtlebotwithworld.launch" & 
+sleep 5
+xterm  -e  " roslaunch myrobot navigation.launch" 
